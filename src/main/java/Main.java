095de1de12b5d4ba894f8927233cbcf9232eed8d
Kshitij Singh <kshtjkmr10@gmail.com>
2026-05-02 17:29:17 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+
+        byte[] input = System.in.readAllBytes();
+        String command = Arrays.toString(input);
+        System.out.print("{"+command+"}: command not found");
     }
 }
