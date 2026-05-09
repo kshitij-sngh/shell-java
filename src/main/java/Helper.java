80d991dc8a42c0621905a06d9d484650ecd21f30
Kshitij Singh <kshtjkmr10@gmail.com>
2026-05-09 23:53:11 +0530

    codecrafters submit [skip ci]

--- src/main/java/Helper.java
+++ src/main/java/Helper.java
@@ -27,7 +27,7 @@ public class Helper {
         for(int i=0; i<line.length(); i++)
         {
             ch = line.charAt(i);
-            if('\\'==ch)
+            if('\\'==ch && !insideSingleQuotes)
             {
                 if(i+1<line.length())
                     currentArg.append(line.charAt(i+1));
